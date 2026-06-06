1a28a2bbda67d2c1cfed2d9f64acf14d151168e6
luyb177 <2085661244@qq.com>
2026-06-07 00:01:39 +0800

    fix:添加gorm标签字段

--- internal/repo/like/like.go
+++ internal/repo/like/like.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Like struct {
-	ID        uint64 `gorm:"primarykey"`
-	CreatedAt time.Time
-	DeletedAt soft_delete.DeletedAt `gorm:"softDelete:nano"`
+	ID        uint64                `gorm:"primarykey;type:bigint unsigned auto_increment"`
+	CreatedAt time.Time             `gorm:"type:datetime(3)"`
+	DeletedAt soft_delete.DeletedAt `gorm:"softDelete:nano;type:bigint unsigned"`
 
-	TargetType uint8  `gorm:"uniqueIndex:idx_target_user"`
-	TargetID   uint64 `gorm:"uniqueIndex:idx_target_user"`
-	UserID     uint64 `gorm:"uniqueIndex:idx_target_user"`
+	TargetType uint8  `gorm:"uniqueIndex:idx_target_user;type:tinyint unsigned"`
+	TargetID   uint64 `gorm:"uniqueIndex:idx_target_user;type:bigint unsigned"`
+	UserID     uint64 `gorm:"uniqueIndex:idx_target_user;type:bigint unsigned"`
 }
 
 func (Like) TableName() string {
